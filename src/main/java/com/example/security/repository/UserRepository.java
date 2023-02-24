062a033643c0fd7babd2e69be4811e06296710b1
jelena.mikelsone <jelena.mikelsone@accenture.com>
2023-02-24 16:23:34 +0200

    Athentication working

---
+++ src/main/java/com/example/security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.security.repository;
+
+import java.util.Optional;
+
+import com.example.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
